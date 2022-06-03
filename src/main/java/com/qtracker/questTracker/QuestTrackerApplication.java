e59de375d99b4e122b35c1bb7a5092e3b49dbf8a
IgorAntrov <igor3377@yandex.ru>
2022-06-03 15:04:43 +0500

    Initial commit

---
+++ src/main/java/com/qtracker/questTracker/QuestTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.qtracker.questTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qtracker/questTracker/QuestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qtracker.questTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
